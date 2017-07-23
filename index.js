dc75568f924eac861bf8c74ce191e572491bcbf1
gabrielsclimaco <gabrielsclimaco@gmail.com>
2017-07-23 12:05:40 -0300

    🚀 Adds package.json for deploying in heroku

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 var express = require('express')
 var app = express()
 
+const port = process.env.PORT || 3000
+
 app.use(express.static(__dirname));
 app.set('views', __dirname)
 app.engine('html', require('ejs').renderFile)
 app.set('view engine', 'html')
 
 app.get('/', (req, res) => {
+  console.log('game started')
   res.render('index.html')
 })
 
-app.listen(process.env.PORT || 3000)
+app.listen(port, () => {
+  console.log('Listening in port', port)
+})
